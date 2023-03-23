b581bd16811db9dff16851e5a304805fce0a9eb4
Yacine Samba <ysambamezaache@gmail.com>
2023-03-23 03:20:41 +0100

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      "primary": '#0D0D2B',
+      "secondary": '#2B076E',
+      "accent": '#3671E9',
+      "white": '#FFFFFF',
+      "white10": '#ffffff0a',
+      "grey0": '#F2F2F2',
+      "grey1": '#E0E0E0',
+      "grey2": '#BDBDBD',
+      "grey3": '#828282'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
